b5d2cbb10bbac4c578780498aa2131b753101a0c
wel15007 <wel15007@byui.edu>
2021-01-11 14:45:29 -0700

    Finished website

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,1 +1,7 @@
-var string = document.lastModified;
+window.addEventListener("load", (event)=> {
+    const updated = document.querySelector("#lastUpdatedDate");
+    updated.textContent = document.lastModified;
+
+    const copy = document.querySelector("#copyrightYear");
+    copy.textContent = new Date().getFullYear();
+})
