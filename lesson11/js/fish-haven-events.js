1fbc439359b6c2f0e9e4cde38525125ee10e8bb2
wel15007 <wel15007@byui.edu>
2021-03-22 17:09:36 -0600

    Create Account ICE

--- lesson11/js/fish-haven-events.js
+++ lesson11/js/fish-haven-events.js
@@ -5,26 +5,26 @@ window.addEventListener('load', ()=>{
             return response.json();
         })
         .then((jsObject)=> {
-            Object.entries(jsObject).forEach(([key, preston])=> {
-                buildEventsCard(preston);
+            Object.entries(jsObject).forEach(([key, fishHaven])=> {
+                buildEventsCard(fishHaven);
             });
         });
 });
-function buildEventsCard(preston) {
-    console.log(preston);
-    for (let i = 0; i < preston.length; i++){
-        if(preston[i].name === "Preston"){
+function buildEventsCard(fishHaven) {
+    console.log(fishHaven);
+    for (let i = 0; i < fishHaven.length; i++){
+        if(fishHaven[i].name === "Fish Haven"){
             let section = document.createElement("section");
             let div = document.createElement("div");
             let h2 = document.createElement("h2");
             let hr = document.createElement("hr");
-            h2.textContent = "Events in " + preston[i].name;
+            h2.textContent = "Events in " + fishHaven[i].name;
             section.appendChild(h2);
             section.appendChild(hr);
             div.setAttribute('id', "eventInfo");
-            for(let j = 0; j < preston[i].events.length; j++) {
+            for(let j = 0; j < fishHaven[i].events.length; j++) {
                 let p = document.createElement("p");
-                p.textContent = preston[i].events[j];
+                p.textContent = fishHaven[i].events[j];
                 div.appendChild(p);
             }
             document.querySelector("#events").appendChild(section);
--- lesson11/js/soda-springs-events.js
+++ lesson11/js/soda-springs-events.js
@@ -5,26 +5,26 @@ window.addEventListener('load', ()=>{
             return response.json();
         })
         .then((jsObject)=> {
-            Object.entries(jsObject).forEach(([key, preston])=> {
-                buildEventsCard(preston);
+            Object.entries(jsObject).forEach(([key, sodaSprings])=> {
+                buildEventsCard(sodaSprings);
             });
         });
 });
-function buildEventsCard(preston) {
-    console.log(preston);
-    for (let i = 0; i < preston.length; i++){
-        if(preston[i].name === "Preston"){
+function buildEventsCard(sodaSprings) {
+    console.log(sodaSprings);
+    for (let i = 0; i < sodaSprings.length; i++){
+        if(sodaSprings[i].name === "Soda Springs"){
             let section = document.createElement("section");
             let div = document.createElement("div");
             let h2 = document.createElement("h2");
             let hr = document.createElement("hr");
-            h2.textContent = "Events in " + preston[i].name;
+            h2.textContent = "Events in " + sodaSprings[i].name;
             section.appendChild(h2);
             section.appendChild(hr);
             div.setAttribute('id', "eventInfo");
-            for(let j = 0; j < preston[i].events.length; j++) {
+            for(let j = 0; j < sodaSprings[i].events.length; j++) {
                 let p = document.createElement("p");
-                p.textContent = preston[i].events[j];
+                p.textContent = sodaSprings[i].events[j];
                 div.appendChild(p);
             }
             document.querySelector("#events").appendChild(section);
